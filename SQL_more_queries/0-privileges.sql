817f51dd2da3fcddb31bbaacead3910af2c73386
Ange-Mukundente <a.mukundent@alustudent.com>
2023-08-03 08:15:25 +0000

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- list all privlage for user user_0d_1 user_0d_2
-
+-- Lists all privileges of the MySQL Users `user_0d_1`
+-- and `user_0d_2` in `localhost`.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
